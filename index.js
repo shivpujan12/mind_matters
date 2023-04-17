58b89f994b2d6c2f6015bd392fd97118a864cd06
Homoeractus <ajay1882000@gmail.com>
2023-04-18 00:32:28 +0530

    added new files and images.

--- index.js
+++ index.js
@@ -20,15 +20,17 @@ function updateTimer() {
         '<div class="time">' + h + '<span>hours</span></div>' +
         '<div class="time">' + m + '<span>minutes</span></div>' +
         '<div class="time">' + s + '<span>seconds</span></div>' ;
-        d=0;h=0;m=0;s=0;
+        d=0 ;h=0 ; m=0 ;s=0;
         if(d==0 && h==0 && m==0 && s==0)
         {
            clearInterval(myInterval);   //to clear our interval when time is over
-           show_Instruction();    
+           show_Instruction();
         }
 
 }
-function show_Instruction()
-{ //jumps to instruction page
-    console.log("welcome to instructions");
+function show_Instruction(){ //jumps to instruction page
+    document.querySelector('.arrow_img').addEventListener("click",function(){
+        var new_window=window.open('instruction.html','fullscreen'); //self will help to open new html file without opening new window.
+        new_window.resizeTo(screen.availWidth, screen.availHeight);
+    });
 }
