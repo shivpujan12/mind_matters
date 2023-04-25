c30b001d83869276d00d811dce567e651827b512
Homoeractus <ajay1882000@gmail.com>
2023-04-26 00:11:58 +0530

    added confirmation pages (working application)

--- scripts/instruction.js
+++ scripts/instruction.js
@@ -3,15 +3,5 @@ document.querySelector(".submit").addEventListener("click",function(){
     let t1=document.querySelector(".t1").value;
     let t2=document.querySelector(".t2").value;
     let tnum=document.querySelector(".tnum").value;
-    teamname+=" ";
-    t1+=" ";
-    t2+=" ";
-    tnum+=" ";
-    console.log(teamname,t1,t2,tnum);
-    window.open("../pages/confirmation.html","_self");
-    const pass_values=[teamname,t1,t2,tnum];
-    valueSender(pass_values);
+    window.open("../pages/confirmation.html?"+teamname+" "+t1+" "+t2+" "+tnum,"_parent");
 })
-function valueSender(pass_values)
-{
-}
