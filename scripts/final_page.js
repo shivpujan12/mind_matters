fed86de4acafb9e0f2420bb567bad92a62d8b789
Shivpujan yadav <31546372+shivpujan12@users.noreply.github.com>
2023-04-29 02:44:06 +0530

    Done with the final page

--- scripts/final_page.js
+++ scripts/final_page.js
@@ -5,3 +5,7 @@ var score = localStorage['score'] || 0;
 score = parseInt(score);
 console.log(score)
 document.getElementById('final-score').innerText = score;
+
+document.getElementById('data').innerHTML =
+    "<b>Team name:  " + team_name + "<br>" +
+    "<b>Contact: " + contact  ;
