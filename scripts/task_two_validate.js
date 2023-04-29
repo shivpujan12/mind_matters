33fc20c11ee35ac6a3acd35ef60efc889900c01a
Shivpujan yadav <31546372+shivpujan12@users.noreply.github.com>
2023-04-29 16:42:57 +0530

    task 2 answers done

--- scripts/task_two_validate.js
+++ scripts/task_two_validate.js
@@ -18,7 +18,7 @@ function checkQuestion1() {
     const data = document.querySelector('input[name="option_question_1"]:checked');
     if (data === null) {
         console.log("Not selected any option");
-    } else if (data.value === "Option 2") {
+    } else if (data.value === "FORmula TRANslation") {
         console.log("Correct Option");
         score = score + 2;
     } else {
@@ -32,7 +32,7 @@ function checkQuestion2() {
     const data = document.querySelector('input[name="option_question_2"]:checked');
     if (data === null) {
         console.log("Not selected any option");
-    } else if (data.value === "Option 2") {
+    } else if (data.value === "R") {
         console.log("Correct Option");
         score = score + 2;
     } else {
@@ -46,7 +46,7 @@ function checkQuestion3() {
     const data = document.querySelector('input[name="option_question_3"]:checked');
     if (data === null) {
         console.log("Not selected any option");
-    } else if (data.value === "Option 2") {
+    } else if (data.value === "Rust") {
         console.log("Correct Option");
         score = score + 2;
     } else {
@@ -60,7 +60,7 @@ function checkQuestion4() {
     const data = document.querySelector('input[name="option_question_4"]:checked');
     if (data === null) {
         console.log("Not selected any option");
-    } else if (data.value === "Option 2") {
+    } else if (data.value === "a") {
         console.log("Correct Option");
         score = score + 2;
     } else {
@@ -73,7 +73,7 @@ function checkQuestion5() {
     const data = document.querySelector('input[name="option_question_5"]:checked');
     if (data === null) {
         console.log("Not selected any option");
-    } else if (data.value === "Option 2") {
+    } else if (data.value === "LISP") {
         console.log("Correct Option");
         score = score + 2;
     } else {
